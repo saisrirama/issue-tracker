c4cf2453b79e461dcf72a24580361c23049ee56d
KarlyleFr <karlyle.fr@gmail.com>
2026-03-21 22:32:00 +0000

    changes vite econfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/issue-tracker/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
-            return 'assets/index.css'
-          }
-          return 'assets/[name][extname]'
-        },
-      },
-    },
-  },
 })
