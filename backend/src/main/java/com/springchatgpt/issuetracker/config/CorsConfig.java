45742d1ef3658adb95f04bf703b2b944be3608fb
KarlyleFr <karlyle.fr@gmail.com>
2026-03-26 15:43:11 +0000

    CORS Error Fix

--- backend/src/main/java/com/springchatgpt/issuetracker/config/CorsConfig.java
+++ backend/src/main/java/com/springchatgpt/issuetracker/config/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://saisrirama.github.io"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
