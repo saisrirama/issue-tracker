17067c3e2374dfb49e72ae4fa9fa7b1120f4d78f
KarlyleFr <karlyle.fr@gmail.com>
2026-03-26 15:19:42 +0000

    fixing 405 error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 console.log("ENV URL:", import.meta.env.VITE_API_BASE_URL);
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
+//const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
+const apiBaseUrl = "https://issue-tracker-0z0r.onrender.com";
 console.log("ENV URL:", import.meta.env.VITE_API_BASE_URL);
 const api = axios.create({
   baseURL: `${apiBaseUrl}/api/v1`,
