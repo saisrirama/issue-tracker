32fea3ebcfc912929f5dd21fa7b7fcabd886c4db
KarlyleFr <karlyle.fr@gmail.com>
2026-03-26 15:10:19 +0000

    fix for signup issue

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+console.log("ENV URL:", import.meta.env.VITE_API_BASE_URL);
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
 console.log("ENV URL:", import.meta.env.VITE_API_BASE_URL);
 const api = axios.create({
